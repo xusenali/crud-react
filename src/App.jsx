23762394c5750c8a6b5ff8d28e3df483c0606b42
xusenali <github6121@gmail.com>
2025-05-03 17:38:46 +0300

    work

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useRef, useState } from 'react'
+
 import './App.css'
 function App() {
   let [users, setUsers] = useState(
@@ -10,6 +9,9 @@ function App() {
     JSON.parse(localStorage.getItem('likedUsers') || '[]')
   );
 
+  let [select, setSelect] = useState('all')
+
+  let filter = useRef(null)
 
   function formleft() {
     const form = document.querySelector('form')
@@ -52,9 +54,9 @@ function App() {
 
   function addheart(event) {
     const isHearted = event.target.classList.toggle('text-red-500');
-    
+
     const id = Number(event.target.getAttribute('data-heart-id'));
-  
+
     if (isHearted) {
       users.forEach(element => {
         if (id === element.id) {
@@ -64,15 +66,13 @@ function App() {
             console.log(likedUsers);
             setLikedUsers(likedUsers);
             localStorage.setItem('likedUsers', JSON.stringify(likedUsers));
-            
-            
           }
         }
       });
     } else {
-      removeLikedUsers(id); 
+      removeLikedUsers(id);
     }
-  
+
     function removeLikedUsers(id) {
       likedUsers = likedUsers.filter(user => user.id !== id);
       console.log(likedUsers);
@@ -88,18 +88,25 @@ function App() {
       const isLiked = likedUsers.some(user => user.id === id);
       if (isLiked) {
         heart.classList.add('text-red-500');
-      } 
+      }
     });
   }
- 
+  function selectvalue() {
+    setSelect(filter.current.value)
+
+  }
+  const filteredUsers = select === 'all' ? users : users.filter(item => item.category === select)
   return (
     <>
       <div className='bg-white-400 h-20 w-100% flex justify-between items-center p-5'>
         <h1 className=' text-3xl' >users</h1>
         <div className="icongg flex gap-4">
-          <select name="heartcategory" id="" className='p-3 border-1 rounded'>
-            <option value="All">All</option>
-            <option value="Favorite">Favorite</option>
+          <select ref={filter} onChange={selectvalue} name="heartcategory" id="select" className='p-3 border-1 rounded'>
+            <option value="all">All</option>
+            <option value="family">Family</option>
+            <option value="friends">Friends</option>
+            <option value="relatives">Relatives</option>
+            <option value="other">Other</option>
           </select>
           <button onClick={formleft} id='AddBtn' className='p-2.5 rounded bg-blue-600 text-white'>Add</button>
         </div>
@@ -111,23 +118,24 @@ function App() {
           <li className='text-2xl'>Category</li>
           <li className='text-2xl'>PhoneNumber</li>
         </ul>
-        {users.map((value, index) => {
-          return (
-            < ul key={index + 1} className='p-6 flex justify-between border-b relative ' >
-              <div className="btnlar  absolute right-2 top-2 flex gap-3">
-                <i onClick={addheart} data-heart-id={value.id} className='bx bxs-heart text-gray-400' ></i>
-                <i onClick={deleteUsers} data-id={value.id} className='bx bxs-box text-red-500'></i>
-              </div>
+        {
+          filteredUsers.map((value, index) => {
+            return (
+              < ul key={index + 1} className='p-6 flex justify-between border-b relative ' >
+                <div className="btnlar  absolute right-2 top-2 flex gap-3">
+                  <i onClick={addheart} data-heart-id={value.id} className='bx bxs-heart text-gray-400' ></i>
+                  <i onClick={deleteUsers} data-id={value.id} className='bx bxs-box text-red-500'></i>
+                </div>
 
-              <li key={value.firstname} className='text-2xl'>{value.firstname}</li>
-              <li key={value.lastname} className='text-2xl'>{value.lastname}</li>
-              <li key={value.category} className='text-2xl'>{value.category}</li>
-              <li key={value.phoneNum} className='text-2xl'>{value.phoneNum}</li>
-            </ul>
+                <li key={value.firstname} className='text-2xl'>{value.firstname}</li>
+                <li key={value.lastname} className='text-2xl'>{value.lastname}</li>
+                <li key={value.category} className='text-2xl'>{value.category}</li>
+                <li key={value.phoneNum} className='text-2xl'>{value.phoneNum}</li>
+              </ul>
 
-          )
+            )
 
-        })}
+          })}
         <form onSubmit={addusers} className='items-center justify-center gap-5 flex flex-col  w-80 h-100 shadow-2xl shadow-gray-700 absolute right-[-400px] top-0 transition-all ease-in-out duration-300 rounded-2xl bg-white z-3'>
           <input required minLength={4} name='firstname' className='border-1 p-4 rounded' type='text' placeholder='FirstName...' />
           <input required minLength={4} name='lastname' className='border-1 p-4 rounded' type='text' placeholder='LastName...' />
